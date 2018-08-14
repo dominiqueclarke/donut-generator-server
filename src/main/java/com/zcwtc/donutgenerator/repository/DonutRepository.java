0afd53bb0019f4287284d5224bae693c3ae5ec89
Dominique Clarke <dominiquedclarke@gmail.com>
2018-08-13 20:09:42 -0400

    refactored to spring data rest

--- src/main/java/com/zcwtc/donutgenerator/repository/DonutRepository.java
+++ src/main/java/com/zcwtc/donutgenerator/repository/DonutRepository.java
@@ -2,7 +2,11 @@ package com.zcwtc.donutgenerator.repository;
 
 import com.zcwtc.donutgenerator.domains.Donut;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
+@RepositoryRestResource
 public interface DonutRepository extends CrudRepository<Donut, Long> {
 
 }
